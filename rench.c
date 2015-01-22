ab5a089a06c574bdf9b79355fed091c7323b9070
satoshi_shiba <shiba@rvm.jp>
2015-01-23 01:31:21 +0900

    インデントを修正

--- rench.c
+++ rench.c
@@ -9,90 +9,90 @@
 #define DEFAULT_BITRATE (128 * 1024)
 
 typedef struct __options {
-	uint32_t bitrate;
-	char *host;
-	int port;
+  uint32_t bitrate;
+  char *host;
+  int port;
 } options;
 
 void illegal_option(char *errmsg) {
-	fprintf(stderr, "%s\n", errmsg);
-	exit(1);
+  fprintf(stderr, "%s\n", errmsg);
+  exit(1);
 }
 
 void init_options(options *opts) {
-	opts->bitrate = DEFAULT_BITRATE;
-	opts->host = NULL;
-	opts->port = -1;
+  opts->bitrate = DEFAULT_BITRATE;
+  opts->host = NULL;
+  opts->port = -1;
 }
 
 void set_bitrate_option(options *opts, char *bitrate) {
-	int l = strlen(bitrate);
-	int b = atoi(bitrate);
-	if (b <= 0) {
-		illegal_option("bitrate must be grater than 0.");
-	}
-	switch (bitrate[l - 1]) {
-		case 'M':
-		case 'm':
-			b *= 1024;
-		case 'k':
-		case 'K':
-			b *= 1024;
-	}
-	opts->bitrate = b;
+  int l = strlen(bitrate);
+  int b = atoi(bitrate);
+  if (b <= 0) {
+    illegal_option("bitrate must be grater than 0.");
+  }
+  switch (bitrate[l - 1]) {
+    case 'M':
+    case 'm':
+      b *= 1024;
+    case 'k':
+    case 'K':
+      b *= 1024;
+  }
+  opts->bitrate = b;
 }
 
 void set_host_option(options *opts, char *host) {
-	int l = strlen(host);
-	if (l == 0) {
-		illegal_option("invalid hostname");
-	}
-	opts->host = malloc(l + 1);
-	strcpy(opts->host, host);
+  int l = strlen(host);
+  if (l == 0) {
+    illegal_option("invalid hostname");
+  }
+  opts->host = malloc(l + 1);
+  strcpy(opts->host, host);
 }
 
 void set_port_option(options *opts, char *port) {
-	int p = atoi(port);
-	if (p < 0) {
-		illegal_option("port number must be grater than 0.");
-	}
-	opts->port = p;
+  int p = atoi(port);
+  if (p < 0) {
+    illegal_option("port number must be grater than 0.");
+  }
+  opts->port = p;
 }
 
 void parse_options(options *opts, int argc, char *argv[]) {
-	int opt;
-	while ((opt = getopt(argc, argv, "h:p:b:")) != -1) {
-		switch (opt) {
-			case 'b':
-				set_bitrate_option(opts, optarg);
-				break;
-			case 'h':
-				set_host_option(opts, optarg);
-				break;
-			case 'p':
-				set_port_option(opts, optarg);
-				break;
-		}
-	}
+  int opt;
+  while ((opt = getopt(argc, argv, "h:p:b:")) != -1) {
+    switch (opt) {
+      case 'b':
+        set_bitrate_option(opts, optarg);
+        break;
+      case 'h':
+        set_host_option(opts, optarg);
+        break;
+      case 'p':
+        set_port_option(opts, optarg);
+        break;
+    }
+  }
 }
 
 void validate_options(options *opts) {
-	if (!opts->host) {
-		illegal_option("host is not given.");
-	}
-	if (opts->port < 0) {
-		illegal_option("port is not given.");
-	}
+  if (!opts->host) {
+    illegal_option("host is not given.");
+  }
+  if (opts->port < 0) {
+    illegal_option("port is not given.");
+  }
 }
 
 int main(int argc, char *argv[]) {
-	options opts;
+  options opts;
 
-	init_options(&opts);
-	parse_options(&opts, argc, argv);
-	validate_options(&opts);
+  init_options(&opts);
+  parse_options(&opts, argc, argv);
+  validate_options(&opts);
 
-	fprintf(stderr, "target = %s:%u, bitrate = %u\n", opts.host, (uint16_t)opts.port, opts.bitrate);
+  fprintf(stderr, "target = %s:%u, bitrate = %u\n", opts.host, (uint16_t)opts.port, opts.bitrate);
 
-	return 0;
+  return 0;
 }
