0081786533f8451c752c48f80e125be8ea0a6e61
satoshi_shiba <shiba@rvm.jp>
2015-01-23 01:54:53 +0900

    指定したファイルを open する

--- rench.c
+++ rench.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "rench.h"
+
+void run(char *file) {
+  int fd = open(file, O_RDONLY);
+  if (fd < 0) {
+    fprintf(stderr, "failed to open %s: ", file);
+    perror(NULL);
+    goto out;
+  }
+
+out:
+  if (fd >= 0) {
+    close(fd);
+  }
+}
 
 int main(int argc, char *argv[]) {
   options opts;
@@ -13,6 +27,7 @@ int main(int argc, char *argv[]) {
   validate_options(&opts);
 
   fprintf(stderr, "file = %s, bitrate = %u\n", opts.file, opts.bitrate);
+  run(opts.file);
 
   free_options(&opts);
 
