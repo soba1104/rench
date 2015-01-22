9f9954d1f7ca50b5b2630ae85037bde948477bd7
satoshi_shiba <shiba@rvm.jp>
2015-01-23 02:06:29 +0900

    バッファサイズを指定できるようにする

--- options.c
+++ options.c
@@ -12,6 +12,7 @@ void illegal_option(char *errmsg) {
 
 void init_options(options *opts) {
   opts->bitrate = DEFAULT_BITRATE;
+  opts->bufsize = DEFAULT_BUFSIZE;
   opts->file = NULL;
 }
 
@@ -32,6 +33,23 @@ void set_bitrate_option(options *opts, char *bitrate) {
   opts->bitrate = b;
 }
 
+void set_bufsize_option(options *opts, char *bufsize) {
+  int l = strlen(bufsize);
+  int s = atoi(bufsize);
+  if (s <= 0) {
+    illegal_option("buffer size must be grater than 0.");
+  }
+  switch (bufsize[l - 1]) {
+    case 'M':
+    case 'm':
+      s *= 1024;
+    case 'k':
+    case 'K':
+      s *= 1024;
+  }
+  opts->bufsize = s;
+}
+
 void set_file_option(options *opts, char *file) {
   int l = strlen(file);
   if (l == 0) {
@@ -43,10 +61,13 @@ void set_file_option(options *opts, char *file) {
 
 void parse_options(options *opts, int argc, char *argv[]) {
   int opt;
-  while ((opt = getopt(argc, argv, "f:b:")) != -1) {
+  while ((opt = getopt(argc, argv, "s:f:b:")) != -1) {
     switch (opt) {
       case 'b':
         set_bitrate_option(opts, optarg);
+        break;
+      case 's':
+        set_bufsize_option(opts, optarg);
         break;
       case 'f':
         set_file_option(opts, optarg);
--- rench.h
+++ rench.h
@@ -1,12 +1,12 @@
 #include <inttypes.h>
 
 #define DEFAULT_BITRATE (128 * 1024)
+#define DEFAULT_BUFSIZE (10 * 1024 * 1024)
 
 typedef struct __options {
   uint32_t bitrate;
+  uint32_t bufsize;
   char *file;
-  char *host;
-  int port;
 } options;
 
 
