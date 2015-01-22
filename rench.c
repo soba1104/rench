717e29d8111ce2e80803b10d8913d98ec637d55e
satoshi_shiba <shiba@rvm.jp>
2015-01-23 02:41:48 +0900

    ファイルからの読み込み処理を追加

--- rench.c
+++ rench.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <assert.h>
 
 #include "rench.h"
 
-void run(char *file) {
-  int fd = open(file, O_RDONLY);
+void loop(int fd, char *buf, uint32_t bufsize, uint32_t bitrate) {
+  int bufidx = 0;
+  int progress = 0;
+
+  while (1) {
+    struct timeval starttime, endtime;
+    int toread = bufsize - bufidx;
+    uint64_t usec;
+    assert(toread >= 0);
+
+    gettimeofday(&starttime, NULL);
+    if (toread > 0) { // buffer remains toread bytes.
+      int ret = read(fd, buf+bufidx, toread);
+      if (!ret) { // EOF
+        return;
+      } else if (ret < 0) {
+        perror("failed to read: ");
+      }
+      bufidx += ret;
+    } else { // toread == 0, buffer full.
+      bufidx -= bitrate;
+      sleep(1);
+    }
+    gettimeofday(&endtime, NULL);
+
+    usec = (endtime.tv_sec * 1000000 + endtime.tv_usec) - (starttime.tv_sec * 1000000 + starttime.tv_usec);
+    fprintf(stderr, "usec = %ld\n", usec);
+  }
+}
+
+void run(options *opts) {
+  char *file = opts->file;
+  char *buf;
+  uint32_t bufsize = opts->bufsize;
+  uint32_t bitrate = opts->bitrate;
+  int fd;
+
+  buf = malloc(bufsize);
+  if (!buf) {
+    fprintf(stderr, "failed to allocate buffer\n");
+    goto out;
+  }
+
+  fd = open(file, O_RDONLY);
   if (fd < 0) {
     fprintf(stderr, "failed to open %s: ", file);
     perror(NULL);
     goto out;
   }
 
+  loop(fd, buf, bufsize, bitrate);
+
 out:
+  if (buf) {
+    free(buf);
+  }
   if (fd >= 0) {
     close(fd);
   }
@@ -27,7 +76,7 @@ int main(int argc, char *argv[]) {
   validate_options(&opts);
 
   fprintf(stderr, "file = %s, bitrate = %u, bufsize = %u\n", opts.file, opts.bitrate, opts.bufsize);
-  run(opts.file);
+  run(&opts);
 
   free_options(&opts);
 
